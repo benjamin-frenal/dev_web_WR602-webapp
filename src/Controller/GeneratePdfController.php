f1abdc957221b5530efad67d07ee0a6df8dca1ba
Benjamin Frenal <benjamin.frenal@etudiant.univ-reims.fr>
2024-05-23 14:28:41 +0200

    ✨ Feat : Voir la génération des pdfs

--- src/Controller/GeneratePdfController.php
+++ src/Controller/GeneratePdfController.php
@@ -10,9 +10,15 @@ use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class GeneratePdfController extends AbstractController
 {
+    private $params;
+    public function __construct(ParameterBagInterface $params)
+    {
+        $this->params = $params;
+    }
     #[Route('/generate/pdf', name: 'app_generate_pdf')]
     public function generatePdf(Request $request, EntityManagerInterface $entityManager): Response
     {
@@ -28,11 +34,11 @@ class GeneratePdfController extends AbstractController
             // Créer une instance HttpClient
             $client = HttpClient::create();
 
+            $microServiceUrl = $this->params->get('micro_service_url');
+
             // Envoyer la requête POST à l'URL spécifiée avec le paramètre 'url'
-            $response = $client->request('POST', 'https://127.0.0.1:8001/gotenberg/convert', [
-                'headers' => [
-                    'Content-Type' => 'multipart/form-data',
-                ],
+            $response = $client->request('POST', $microServiceUrl, [
+
                 'body' => [
                     'url' => $url,
                 ]
@@ -54,17 +60,35 @@ class GeneratePdfController extends AbstractController
             // add the pdf in the pdf entity
             $pdf = new Pdf();
             $pdf->setTitle($pdfName);
+            $pdf->setOwner($this->getUser());
             $pdf->setCreatedAt(new \DateTimeImmutable());
             $pdf->setPath($filePath);
 
             // save the pdf in the database
             $entityManager->persist($pdf);
             $entityManager->flush();
+
+            // Redirect to the PDF display route
+            return $this->redirectToRoute('app_show_pdf', ['id' => $pdf->getId()]);
         }
 
         // Afficher le formulaire
         return $this->render('generate_pdf/index.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/generate/pdf/{id}', name: 'app_show_pdf')]
+    public function showPdf(int $id, EntityManagerInterface $entityManager): Response
+    {
+        $pdf = $entityManager->getRepository(Pdf::class)->find($id);
+
+        if (!$pdf) {
+            throw $this->createNotFoundException('The PDF does not exist');
+        }
+
+        return $this->render('generate_pdf/show.html.twig', [
+            'pdf' => $pdf,
         ]);
     }
 }
